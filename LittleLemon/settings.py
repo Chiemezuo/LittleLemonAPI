dd75b4f89f6d56e502cc74b98060fb2766c3178a
Chiemezuo <chiemezuoakujobi@gmail.com>
2023-12-12 09:08:29 +0100

    make restaurant app

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'LittleLemonAPI',
-    'djoser'
+    'djoser',
+    'restaurant',
 ]
 
 MIDDLEWARE = [
@@ -79,8 +80,12 @@ WSGI_APPLICATION = 'LittleLemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'littlelemon',
+        'HOST': '127.0.0.1',
+        'USER': 'root',
+        'PASSWORD': 'Chiemezuo',
+        'PORT': '3306',
     }
 }
 
